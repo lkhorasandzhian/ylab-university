d4441d4f3314080e0cb0aa325d0d96398dc8a93e
Levon Khorasandzhian <levon.kh228@gmail.com>
2025-11-15 19:51:18 +0300

    Remove old generated javadoc folder

--- homework-1/javadoc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html","k":"18"},{"l":"ru.ylab.levon"},{"l":"ru.ylab.levon.model"},{"l":"ru.ylab.levon.service"},{"l":"ru.ylab.levon.storage"},{"l":"ru.ylab.levon.view"}];updateSearchResults();
