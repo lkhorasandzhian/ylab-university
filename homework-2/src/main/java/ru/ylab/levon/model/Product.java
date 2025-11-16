74da9fc6922defc7ffe582e0b98d1e9a15dcded1
Levon Khorasandzhian <levon.kh228@gmail.com>
2025-11-16 19:57:09 +0300

    Delete redundant validations (already exist in CatalogService)

--- homework-2/src/main/java/ru/ylab/levon/model/Product.java
+++ homework-2/src/main/java/ru/ylab/levon/model/Product.java
@@ -2,16 +2,14 @@ package ru.ylab.levon.model;
 
 import java.math.BigDecimal;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
-import lombok.NonNull;
-import lombok.AllArgsConstructor;
+import lombok.*;
 
 @Getter
+@Setter
 @ToString
 @AllArgsConstructor
 public class Product {
+    @Setter(AccessLevel.NONE)
     private Long id;
     private @NonNull String name;
     private @NonNull String category;
@@ -25,33 +23,5 @@ public class Product {
             throw new IllegalStateException("Существующий ID не может быть изменён");
         }
         this.id = id;
-    }
-
-    public void setName(@NonNull String name) {
-        if (name.isBlank()) {
-            throw new IllegalArgumentException("Название не может быть пустым");
-        }
-        this.name = name;
-    }
-
-    public void setCategory(@NonNull String category) {
-        if (category.isBlank()) {
-            throw new IllegalArgumentException("Категория не может быть пустой");
-        }
-        this.category = category;
-    }
-
-    public void setBrand(@NonNull String brand) {
-        if (brand.isBlank()) {
-            throw new IllegalArgumentException("Бренд не может быть пустым");
-        }
-        this.brand = brand;
-    }
-
-    public void setPrice(BigDecimal price) {
-        if (price.compareTo(BigDecimal.ZERO) <= 0) {
-            throw new IllegalArgumentException("Цена должна быть положительной");
-        }
-        this.price = price;
     }
 }
