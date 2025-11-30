1c3b84f58d3a2ca0de6f6a904593ea1898c6c7c3
Levon Khorasandzhian <levon.kh228@gmail.com>
2025-11-30 18:57:07 +0300

    Add auth checking for audit

--- homework/src/main/java/ru/ylab/levon/controller/AuditController.java
+++ homework/src/main/java/ru/ylab/levon/controller/AuditController.java
@@ -1,12 +1,13 @@
 package ru.ylab.levon.controller;
 
+import java.util.Map;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.ylab.levon.mapper.AuditMapper;
 import ru.ylab.levon.service.api.AuditService;
-
-import java.util.List;
+import ru.ylab.levon.service.api.UserService;
 
 /**
  * REST-контроллер для работы с журналом аудита.
@@ -24,6 +25,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class AuditController {
     private final AuditService auditService;
+    private final UserService userService;
 
     /**
      * Возвращает список всех записей аудита.
@@ -31,9 +33,16 @@ public class AuditController {
      * @return JSON-массив записей
      */
     @GetMapping
-    public ResponseEntity<List<?>> getAllAuditRecords() {
+    public ResponseEntity<?> getAllAuditRecords() {
+        if (!userService.isLoggedIn()) {
+            return ResponseEntity
+                    .status(401)
+                    .body(Map.of("error", "You must be logged in"));
+        }
+
         var records = auditService.getAll();
         var dtoList = AuditMapper.INSTANCE.toDtoList(records);
+
         return ResponseEntity.ok(dtoList);
     }
 }
