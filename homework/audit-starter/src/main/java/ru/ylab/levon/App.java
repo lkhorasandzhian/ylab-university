5ccd14b87a90b550291c0c2934c2c13b0682ceed
Levon Khorasandzhian <levon.kh228@gmail.com>
2025-12-07 02:43:15 +0300

    Transform project from single-module to multi-module
    Create separate modules: app, audit-starter, logging-starter

---
+++ homework/audit-starter/src/main/java/ru/ylab/levon/App.java
@@ -0,0 +1,13 @@
+package ru.ylab.levon;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
