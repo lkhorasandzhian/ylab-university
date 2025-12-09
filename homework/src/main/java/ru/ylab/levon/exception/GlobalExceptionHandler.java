d6c55f5763232fd4241ab1994dc3c3378d0ac527
Levon Khorasandzhian <levon.kh228@gmail.com>
2025-12-09 17:48:41 +0300

    Fix import by removing wildcard

--- homework/src/main/java/ru/ylab/levon/exception/GlobalExceptionHandler.java
+++ homework/src/main/java/ru/ylab/levon/exception/GlobalExceptionHandler.java
@@ -4,7 +4,8 @@ import java.util.Map;
 
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 
